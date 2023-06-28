c73c8127453a5f4bcb97102c1d1363692e8eb293
basha A <130438084+ambasha7@users.noreply.github.com>
2023-06-28 12:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 // Your Script here.
 
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+function rot13(encodedStr){
+   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for(let i = 0 ; i < encodedStr.length ; i++) {
+        if(lookup[ encodedStr[i] ] === undefined){
+            decoderArr.push( encodedStr[i] ) ;
+        }   
 
-  return; //return decodedArr
+        else{ 
+            decoderArr.push(lookup[ encodedStr[i] ]);
+        }
+
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
